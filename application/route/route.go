fb84d4eff49a8105fff49aa4706490ea62bffaf0
Carlos Henrique <carlos_henrique1313@outlook.com>
2022-10-18 21:22:12 -0300

    finalizando

--- application/route/route.go
+++ application/route/route.go
@@ -1,16 +1,34 @@
 package route
 
-import "errors"
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type Route struct{
-	ID string
-	ClientID String
-	Positions []Position
+	ID 		 	string `json:"routeId"`
+	ClientID 	string `json:"clienteId"`
+	Positions 	[]Position `json:"positions"`
 }
 
 type Position struct {
-	Lat float64
-	Long float64
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
+}
+
+type PartialRoutePosition struct {
+	ID 		 string 	`json:"routeId"`
+	ClientID string 	`json:"clientId"`
+	Position []float64 	`json:"position"`
+	Finished bool 		`json:"finished"`
+}
+
+func NewRoute() *Route {
+	return &Route{}
 }
 
 func(r *Route) LoadPositions() error {
@@ -25,22 +43,50 @@ func(r *Route) LoadPositions() error {
 
 	defer f.Close()
 
-	sanner := buffio.NewScanner(f)
+	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
-		data := strings.Split(sanner.Text(), ",")
-		lat, err := srtconv.ParseFloat(data[0], 64)
+		data := strings.Split(scanner.Text(), ",")
+		lat, err := strconv.ParseFloat(data[0], 64)
 		if err != nil {
 			return nil
 		}
-		long, err := srtconv.ParseFloat(data[1], 64)
+		long, err := strconv.ParseFloat(data[1], 64)
 		if err != nil {
 			return nil
 		}
 		r.Positions = append(r.Positions, Position {
 			Lat: lat,
-			Long: long
+			Long: long,
 		})
 	}
 	return nil
+}
+
+func (r *Route) ExportJsonPositions() ([]string, error) {
+	var route PartialRoutePosition
+	var result []string
+	total := len(r.Positions)
+
+	for k, v := range r.Positions {
+
+		route.ID = r.ID
+		route.ClientID = r.ClientID
+		route.Position = []float64{v.Lat, v.Long}
+		route.Finished = false
+
+		if total-1 == k {
+			route.Finished = true
+		}
+
+		jsonRoute, err := json.Marshal(route)
+
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, string(jsonRoute))
+	}
+
+	return result, nil
 }
